157994333372622ad17250fe8336d44e35877325
BramAristyo <bram.mymen@gmail.com>
2026-05-20 20:45:50 +0700

    Add Haversine distance check to AttendanceSetting
    Introduce IsValidDistance(lat, lng) on AttendanceSetting to validate
    distance
    using the haversine formula against Radius

--- server/internal/domain/attendance_setting.go
+++ server/internal/domain/attendance_setting.go
@@ -1,6 +1,9 @@
 package domain
 
-import "time"
+import (
+	"math"
+	"time"
+)
 
 type AttendanceSetting struct {
 	ID        uint      `gorm:"primaryKey"`
@@ -13,4 +16,21 @@ type AttendanceSetting struct {
 
 func (AttendanceSetting) TableName() string {
 	return "attendance_settings"
+}
+
+func (as *AttendanceSetting) IsValidDistance(lat float64, lng float64) bool {
+	const R = 6371000
+
+	dLat := (lat - as.Lat) * (math.Pi / 180.0)
+	dLng := (lng - as.Lng) * (math.Pi / 180.0)
+
+	lat1 := as.Lat * (math.Pi / 180.0)
+	lat2 := lat * (math.Pi / 180.0)
+
+	a := math.Sin(dLat/2)*math.Sin(dLat/2) +
+		math.Sin(dLng/2)*math.Sin(dLng/2)*math.Cos(lat1)*math.Cos(lat2)
+	c := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
+	dist := R * c
+
+	return dist <= as.Radius
 }
